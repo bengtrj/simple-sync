209797f7bcfeee83ecd62715c0a28e4531507d1d
Bengt Hammarlund <bgadelhahammarlund@pivotal.io>
2020-01-26 19:43:26 +0000

    Update bootstrap.sh to include the build

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,8 @@ sudo apt-get -y upgrade
 wget https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz
 sudo tar -xvf go1.13.3.linux-amd64.tar.gz
 sudo mv go /usr/local
+
+export GOROOT=/usr/local/go
+export PATH=$GOROOT/bin:$PATH
+
+./make.sh
