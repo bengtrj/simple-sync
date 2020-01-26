b09c5dd0a970dcf9d06f915bfce99705dc09e3d8
Bengt Hammarlund <bgadelhahammarlund@pivotal.io>
2020-01-26 19:15:33 +0000

    Add boostrap.sh that installs Go (focus on linux)

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set +eu
+
+sudo apt-get update
+sudo apt-get -y upgrade
+wget https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz
+sudo tar -xvf go1.13.3.linux-amd64.tar.gz
+sudo mv go /usr/local
